c129d676a41c451ad0ed5647ca2df0d4656a81b5
Mauricio Antonio Lefno Tiznado <mauricio.lefno2023@twk.cl>
2024-07-11 01:22:54 -0400

    Agregadas funcionalidades de la navbar incluidas las notificaciones y config

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -10,13 +10,44 @@ import { CommonModule } from '@angular/common';
 })
 export class NavbarComponent {
 
+  notificationsEnabled = true;
+
+  notificaciones = [
+    { id: 1, texto: 'notificacion 1' },
+    { id: 2, texto: 'notificacion 2' },
+    { id: 3, texto: 'notificacion 3' },
+    { id: 4, texto: 'notificacion 4' }
+
+  ];
+
+  deleteNotification(notificationId: number) {
+    const index = this.notificaciones.findIndex(notif => notif.id === notificationId);
+    if (index !== -1) {
+      this.notificaciones.splice(index, 1);
+    }
+  }
+
+  toggleNotifications() {
+    this.notificationsEnabled = !this.notificationsEnabled;
+  }
+
   isOpen = false;
+  isOpenConf = false;
 
   openNot() {
     this.isOpen = true;
+    this.isOpenConf = false;
   }
 
   closeModal() {
     this.isOpen = false;
+  }
+
+  openConf() {
+    this.isOpenConf = true;
+    this.isOpen = false;
+  }
+  closeModalConf() {
+    this.isOpenConf = false;
   }
 }
