2ab3fc92a9a9bf271dd4069682fa81aa5d6ff424
Mauricio Antonio Lefno Tiznado <mauricio.lefno2023@twk.cl>
2024-07-13 23:25:20 -0400

    Pagina admin v2

--- src/app/empresa-detail-admin/empresa-detail-admin.component.ts
+++ src/app/empresa-detail-admin/empresa-detail-admin.component.ts
@@ -2,15 +2,25 @@ import { Component } from '@angular/core';
 import {  Router } from '@angular/router';
 import { CommonModule } from '@angular/common';
 import { EmpresaListComponent } from '../empresa-list/empresa-list.component';
+import { FormsModule } from '@angular/forms';  // Importar FormsModule para usar ngModel
 
 @Component({
   selector: 'app-empresa-detail-admin',
   standalone: true,
-  imports: [CommonModule, EmpresaListComponent],
+  imports: [CommonModule, EmpresaListComponent,FormsModule],
   templateUrl: './empresa-detail-admin.component.html',
   styleUrl: './empresa-detail-admin.component.css'
 })
 export class EmpresaDetailAdminComponent {
+  graficos = [
+    {id:1, texto:'Grafico 1', isSelected:true},
+    {id:2, texto:'Grafico 2', isSelected:true},
+    {id:3, texto:'Grafico 3', isSelected:true},
+    {id:4, texto:'Grafico 4', isSelected:true},
+    {id:5, texto:'Grafico 5', isSelected:true},
+    {id:6, texto:'Grafico 6', isSelected:true},
+  ]
+
   empresas = [
     'Agrícola los Pellines',
     'Agricola Jorge Borgeaud',
@@ -24,7 +34,13 @@ export class EmpresaDetailAdminComponent {
     'Agrícola David Estrada'
   ];
 
-  constructor(public router: Router) {}
+  empresaGraficos: { [key: string]: any[] } = {};
+
+  constructor(public router: Router) {
+    this.empresas.forEach(empresa => {
+      this.empresaGraficos[empresa] = this.graficos.map(grafico => ({ ...grafico }));
+    });
+  }
 
   navigateToDetail(empresa: string) {
     const id = this.empresas.indexOf(empresa);
